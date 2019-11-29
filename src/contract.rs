d13696e3dc8daf4ce6b5467d8fd9d909d8069bd5
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-11-29 18:26:32 +0100

    Rewrote contract to use counter app

--- src/contract.rs
+++ src/contract.rs
@@ -4,26 +4,29 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use snafu::{OptionExt, ResultExt};
 
-use cosmwasm::errors::{ContractErr, ParseErr, Result, SerializeErr, Unauthorized};
+use cosmwasm::errors::{ContractErr, ParseErr, Result, SerializeErr, Unauthorized, Utf8Err};
+use cosmwasm::query::perform_raw_query;
 use cosmwasm::serde::{from_slice, to_vec};
 use cosmwasm::storage::Storage;
-use cosmwasm::types::{CosmosMsg, Params, Response};
+use cosmwasm::types::{Params, QueryResponse, RawQuery, Response};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub verifier: String,
-    pub beneficiary: String,
+    pub count: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub verifier: String,
-    pub beneficiary: String,
-    pub funder: String,
+    pub count: i32,
+    pub owner: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct HandleMsg {}
+#[serde(rename_all = "lowercase")]
+pub enum HandleMsg {
+    Increment {},
+    Reset { count: i32 },
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "lowercase")]
@@ -45,34 +48,55 @@ pub fn init<T: Storage>(store: &mut T, params: Params, msg: Vec<u8>) -> Result<R
     store.set(
         CONFIG_KEY,
         &to_vec(&State {
-            verifier: msg.verifier,
-            beneficiary: msg.beneficiary,
-            funder: params.message.signer,
+            count: msg.count,
+            owner: params.message.signer,
         })
             .context(SerializeErr { kind: "State" })?,
     );
     Ok(Response::default())
 }
 
-pub fn handle<T: Storage>(store: &mut T, params: Params, _: Vec<u8>) -> Result<Response> {
+pub fn handle<T: Storage>(store: &mut T, params: Params, msg: Vec<u8>) -> Result<Response> {
+    let msg: HandleMsg = from_slice(&msg).context(ParseErr { kind: "HandleMsg" })?;
     let data = store.get(CONFIG_KEY).context(ContractErr {
         msg: "uninitialized data",
     })?;
     let state: State = from_slice(&data).context(ParseErr { kind: "State" })?;
 
-    if params.message.signer == state.verifier {
-        let res = Response {
-            messages: vec![CosmosMsg::Send {
-                from_address: params.contract.address,
-                to_address: state.beneficiary,
-                amount: params.contract.balance.unwrap_or_default(),
-            }],
-            log: Some("released funds!".to_string()),
-            data: None,
-        };
-        Ok(res)
-    } else {
+    match msg {
+        HandleMsg::Increment {} => try_increment(store, params, state),
+        HandleMsg::Reset { count } => try_reset(store, params, state, count),
+    }
+}
+
+pub fn try_increment<T: Storage>(
+    store: &mut T,
+    _params: Params,
+    mut state: State,
+) -> Result<Response> {
+    state.count += 1;
+    store.set(
+        CONFIG_KEY,
+        &to_vec(&state).context(SerializeErr { kind: "State" })?,
+    );
+    Ok(Response::default())
+}
+
+pub fn try_reset<T: Storage>(
+    store: &mut T,
+    params: Params,
+    mut state: State,
+    count: i32,
+) -> Result<Response> {
+    if params.message.signer != state.owner {
         Unauthorized {}.fail()
+    } else {
+        state.count = count;
+        store.set(
+            CONFIG_KEY,
+            &to_vec(&state).context(SerializeErr { kind: "State" })?,
+        );
+        Ok(Response::default())
     }
 }
 
@@ -86,27 +110,30 @@ pub fn query<T: Storage>(store: &T, msg: Vec<u8>) -> Result<QueryResponse> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use cosmwasm::errors::Error;
     use cosmwasm::mock::MockStorage;
     use cosmwasm::types::{coin, mock_params};
 
     #[test]
     fn proper_initialization() {
         let mut store = MockStorage::new();
-        let msg = to_vec(&InitMsg {
-            verifier: String::from("verifies"),
-            beneficiary: String::from("benefits"),
-        })
-            .unwrap();
+        let msg = to_vec(&InitMsg { count: 17 }).unwrap();
         let params = mock_params("creator", &coin("1000", "earth"), &[]);
+        // we can just call .unwrap() to assert this was a success
         let res = init(&mut store, params, msg).unwrap();
         assert_eq!(0, res.messages.len());
 
-        // it worked, let's check the state
-        let data = store.get(CONFIG_KEY).expect("no data stored");
-        let state: State = from_slice(&data).unwrap();
-        assert_eq!(state.verifier, String::from("verifies"));
-        assert_eq!(state.beneficiary, String::from("benefits"));
-        assert_eq!(state.funder, String::from("creator"));
+        // it worked, let's query the state
+        let mut q_res = query(&store, raw_query(CONFIG_KEY).unwrap()).unwrap();
+        let model = q_res.results.pop().expect("no data stored");
+        let state: State = from_slice(&model.val).unwrap();
+        assert_eq!(
+            state,
+            State {
+                owner: String::from("creator"),
+                count: 17,
+            }
+        );
     }
 
     #[test]
@@ -119,72 +146,61 @@ mod tests {
     }
 
     #[test]
-    fn proper_handle() {
+    fn increment() {
         let mut store = MockStorage::new();
-
-        // initialize the store
-        let init_msg = to_vec(&InitMsg {
-            verifier: String::from("verifies"),
-            beneficiary: String::from("benefits"),
-        })
-            .unwrap();
-        let init_params = mock_params("creator", &coin("1000", "earth"), &coin("1000", "earth"));
-        let init_res = init(&mut store, init_params, init_msg).unwrap();
-        assert_eq!(0, init_res.messages.len());
+        let msg = to_vec(&InitMsg { count: 17 }).unwrap();
+        let params = mock_params("creator", &coin("2", "token"), &coin("2", "token"));
+        let _res = init(&mut store, params, msg).unwrap();
 
         // beneficiary can release it
-        let handle_params = mock_params("verifies", &coin("15", "earth"), &coin("1015", "earth"));
-        let handle_res = handle(&mut store, handle_params, Vec::new()).unwrap();
-        assert_eq!(1, handle_res.messages.len());
-        let msg = handle_res.messages.get(0).expect("no message");
-        match &msg {
-            CosmosMsg::Send {
-                from_address,
-                to_address,
-                amount,
-            } => {
-                assert_eq!("cosmos2contract", from_address);
-                assert_eq!("benefits", to_address);
-                assert_eq!(1, amount.len());
-                let coin = amount.get(0).expect("No coin");
-                assert_eq!(coin.denom, "earth");
-                assert_eq!(coin.amount, "1015");
-            }
-            _ => panic!("Unexpected message type"),
-        }
+        let params = mock_params("anyone", &coin("2", "token"), &[]);
+        let msg = r#"{"increment":{}}"#.as_bytes();
+        let _res = handle(&mut store, params, msg.to_vec()).unwrap();
 
-        // it worked, let's check the state
-        let data = store.get(CONFIG_KEY).expect("no data stored");
-        let state: State = from_slice(&data).unwrap();
-        assert_eq!(state.verifier, String::from("verifies"));
-        assert_eq!(state.beneficiary, String::from("benefits"));
-        assert_eq!(state.funder, String::from("creator"));
+        // should increase counter by 1
+        let mut q_res = query(&store, raw_query(CONFIG_KEY).unwrap()).unwrap();
+        let model = q_res.results.pop().expect("no data stored");
+        let state: State = from_slice(&model.val).unwrap();
+        assert_eq!(
+            state,
+            State {
+                owner: String::from("creator"),
+                count: 18,
+            }
+        );
     }
 
     #[test]
-    fn failed_handle() {
+    fn reset() {
         let mut store = MockStorage::new();
-
-        // initialize the store
-        let init_msg = to_vec(&InitMsg {
-            verifier: String::from("verifies"),
-            beneficiary: String::from("benefits"),
-        })
-            .unwrap();
-        let init_params = mock_params("creator", &coin("1000", "earth"), &coin("1000", "earth"));
-        let init_res = init(&mut store, init_params, init_msg).unwrap();
-        assert_eq!(0, init_res.messages.len());
+        let msg = to_vec(&InitMsg { count: 17 }).unwrap();
+        let params = mock_params("creator", &coin("2", "token"), &coin("2", "token"));
+        let _res = init(&mut store, params, msg).unwrap();
 
         // beneficiary can release it
-        let handle_params = mock_params("benefits", &[], &coin("1000", "earth"));
-        let handle_res = handle(&mut store, handle_params, Vec::new());
-        assert!(handle_res.is_err());
+        let unauth_params = mock_params("anyone", &coin("2", "token"), &[]);
+        let msg = r#"{"reset":{"count": 5}}"#.as_bytes();
+        let res = handle(&mut store, unauth_params, msg.to_vec());
+        match res {
+            Err(Error::Unauthorized{..}) => {},
+            _ => panic!("Must return unauthorized error"),
+        }
 
-        // state should not change
-        let data = store.get(CONFIG_KEY).expect("no data stored");
-        let state: State = from_slice(&data).unwrap();
-        assert_eq!(state.verifier, String::from("verifies"));
-        assert_eq!(state.beneficiary, String::from("benefits"));
-        assert_eq!(state.funder, String::from("creator"));
+        // only the original creator can reset the counter
+        let auth_params = mock_params("creator", &coin("2", "token"), &[]);
+        let msg = to_vec(&HandleMsg::Reset {count: 5}).unwrap();
+        let _res = handle(&mut store, auth_params, msg).unwrap();
+
+        // should increase counter by 1
+        let mut q_res = query(&store, raw_query(CONFIG_KEY).unwrap()).unwrap();
+        let model = q_res.results.pop().expect("no data stored");
+        let state: State = from_slice(&model.val).unwrap();
+        assert_eq!(
+            state,
+            State {
+                owner: String::from("creator"),
+                count: 5,
+            }
+        );
     }
 }
