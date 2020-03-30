932fe71560fde5d169a9eacff961223421e2662b
Simon Warta <simon@warta.it>
2020-03-30 16:02:21 +0200

    Serde: use snake_case instead of lowercase

--- src/msg.rs
+++ src/msg.rs
@@ -7,14 +7,14 @@ pub struct InitMsg {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
     Increment {},
     Reset { count: i32 },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
     GetCount {},
