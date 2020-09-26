145815dcd8afb0a6ffe4f331a669277ad885594f
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-09-26 11:55:41 +0200

    Add library feature flag, clean up Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,9 @@ pub mod error;
 pub mod msg;
 pub mod state;
 
-#[cfg(target_arch = "wasm32")]
+// comment this out and use the lower form if the contract supports migrations
+#[cfg(all(target_arch = "wasm32", not(feature = "library")))]
 cosmwasm_std::create_entry_points!(contract);
+
+// #[cfg(all(target_arch = "wasm32", not(feature = "library")))]
+// cosmwasm_std::create_entry_points_with_migration!(contract);
