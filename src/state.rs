35781eda7a1318129c36d090d14ed3d23567f9cd
orkunkl <kulceorkun@gmail.com>
2021-05-17 10:57:19 +0300

    Use cw-storage-plus

--- src/state.rs
+++ src/state.rs
@@ -4,18 +4,10 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::{Addr, Storage};
 use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 
-pub static CONFIG_KEY: &[u8] = b"config";
-
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub count: i32,
     pub owner: Addr,
 }
 
-pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
-    singleton(storage, CONFIG_KEY)
-}
-
-pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
-    singleton_read(storage, CONFIG_KEY)
-}
+const STATE: Item<State> = Item::new("state");
