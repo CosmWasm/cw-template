e58dcf4b9ab495245c7e23363c76a40ecb3da165
Mauro Lacy <mauro@lacy.com.es>
2021-08-11 09:43:08 +0200

    Add Response attributes

--- src/contract.rs
+++ src/contract.rs
@@ -15,11 +15,15 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     let state = State {
         count: msg.count,
-        owner: info.sender,
+        owner: info.sender.clone(),
     };
     STATE.save(deps.storage, &state)?;
 
-    Ok(Response::new())
+    Ok(Response::new().add_attributes([
+        ("method", "instantiate"),
+        ("owner", &info.sender.to_string()),
+        ("count", &msg.count.to_string()),
+    ]))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -41,9 +45,8 @@ pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
         Ok(state)
     })?;
 
-    Ok(Response::new())
+    Ok(Response::new().add_attribute("method", "try_increment"))
 }
-
 pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
     STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
         if info.sender != state.owner {
@@ -52,7 +55,7 @@ pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Respons
         state.count = count;
         Ok(state)
     })?;
-    Ok(Response::new())
+    Ok(Response::new().add_attribute("method", "reset"))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
