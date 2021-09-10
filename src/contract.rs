401f3dcb1a1ab8c793c3ea445d4751fafc1aac9f
Jakub Bogucki <jakub@confio.gmbh>
2021-09-10 11:50:00 +0200

    Bump cosmwasm-std and add set_contract_version

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,15 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:{{project-name}}";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -17,6 +22,7 @@ pub fn instantiate(
         count: msg.count,
         owner: info.sender.clone(),
     };
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     STATE.save(deps.storage, &state)?;
 
     Ok(Response::new()
