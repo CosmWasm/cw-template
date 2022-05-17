39eef2fe53dbba6105f0d15eb9f7cab088d1a6a7
Orkun Külçe <orkunkl@users.noreply.github.com>
2022-05-17 13:01:57 +0300

    Upgrade template to 1.0 (#113)
    * Update template to 1.0
    
    * Fix error fmt
    
    * Update cargo.lock
    
    * Update cargo.lock

--- src/error.rs
+++ src/error.rs
@@ -10,8 +10,7 @@ pub enum ContractError {
     Unauthorized {},
 
     #[error("Custom Error val: {val:?}")]
-    CustomError{val: String},
-
+    CustomError { val: String },
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
