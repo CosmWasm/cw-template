7bf1d9ade3e434df7105e4970169c4306f6e8a70
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-07-14 09:04:47 +0700

    fix compile error and schema output

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use {{crate_name}}::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use {{crate_name}}::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
 use {{crate_name}}::state::State;
 
 fn main() {
@@ -16,5 +16,5 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
+    export_schema(&schema_for!(GetCountResponse), &out_dir);
 }
